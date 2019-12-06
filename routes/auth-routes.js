be1e29582ef8fda5b4afb26115d79cdf331cc9f0
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-05 22:56:00 -0800

    create profile route and view

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -19,7 +19,7 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    res.send('you reched the callback URI')
+    res.redirect('/profile')
 })
 
 module.exports = router
