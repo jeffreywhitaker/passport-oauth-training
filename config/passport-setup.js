a7ea1635823a94f0f9ee92a8d84590a228a28566
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-05 22:28:35 -0800

    add serialization and existing user logic

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,29 +1,52 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20')
-const keys = require('./keys')
-const User = require('../models/user-model')
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./keys");
+const User = require("../models/user-model");
+
+passport.serializeUser((user, done) => {
+  // pass on the user id
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  // find user and pass them on
+  User.findById(id).then((user) => {
+    done(null, user)
+  })
+})
 
 passport.use(
-    new GoogleStrategy({
-        // options for the google strategy
-        callbackURL: '/auth/google/redirect',
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret
-    }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
-        console.log('passport callback function fired')
-        console.log(profile)
-        try {
-            thisUser = User.create({
-                username: profile.displayName,
-                googleId: profile.id
-            }).then((thisUser) => { 
-                console.log(thisUser)
-                return thisUser
-            })
+  new GoogleStrategy(
+    {
+      // options for the google strategy
+      callbackURL: "/auth/google/redirect",
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // passport callback function
+      // check if user already exists
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //if they exist, get them
+          console.log("existing user found:", existingUser);
+          done(null, existingUser)
+        } else {
+          // if not, create user in db
+          try {
+            newUser = User.create({
+              username: profile.displayName,
+              googleId: profile.id
+            }).then(newUser => {
+              console.log("New user created:", newUser);
+              done(null, newUser);
+            });
+          } catch (err) {
+            console.log("Error creating user:", err);
+            done(err, null);
+          }
         }
-        catch (err) {
-            console.log(err)
-        }
-    })
-)
+      });
+    }
+  )
+);
