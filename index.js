c8287b41c7096861213b9af4d1b59b7f326d8200
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-05 22:35:47 -0800

    add cookie sessions

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const authRoutes = require('./routes/auth-routes')
 const passportSetup = require('./config/passport-setup')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 
 const app = express();
 
 // set up view engine
 app.set('view engine', 'ejs')
+
+// make the cookie key
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, // one day
+    keys: [keys.session.cookieKey]
+}))
+
+// initialize passport
+app.use(passport.initialize())
+app.use(passport.session())
 
 // connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true }, (err) => {
