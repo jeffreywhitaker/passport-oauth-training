225470a849b92b15a110f0a5f52d313592c252f1
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-09 12:46:13 -0800

    split server.js and set up new imports

--- index.js
+++ index.js
@@ -1,48 +1,6 @@
-const express = require("express");
-const authRoutes = require("./routes/auth-routes");
-const profileRoutes = require("./routes/profile-routes");
-const passportSetup = require("./config/passport-setup");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
+const server = require("./server");
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-
-// set up view engine
-app.set("view engine", "ejs");
-
-// make the cookie key
-app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000, // one day
-    keys: [keys.session.cookieKey]
-  })
-);
-
-// initialize passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// connect to mongodb
-mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true }, err => {
-  if (err) {
-    console.log("mongoose connection failed");
-    console.log(err);
-  } else {
-    console.log("connected to mongodb");
-  }
-});
-
-//set up routes
-app.use("/auth", authRoutes);
-app.use("/profile", profileRoutes);
-
-// create home route
-app.get("/", (req, res) => {
-  res.render("home", { user: req.user });
-});
-
-app.listen(5000, () => {
-  console.log("app now listening for requests on port 5000");
+server.listen(PORT, () => {
+  console.log(`server now listening for requests on port ${PORT}`);
 });
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
+const passportSetup = require("./config/passport-setup");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+const app = express();
+
+// set up view engine
+app.set("view engine", "ejs");
+
+// make the cookie key
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, // one day
+    keys: [keys.session.cookieKey]
+  })
+);
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true }, err => {
+  if (err) {
+    console.log("mongoose connection failed");
+    console.log(err);
+  } else {
+    console.log("connected to mongodb");
+  }
+});
+
+//set up routes
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+
+// create home route
+app.get("/", (req, res) => {
+  res.render("home", { user: req.user });
+});
+
+module.exports = app;
